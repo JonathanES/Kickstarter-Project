c7a5321e9a2ad94cd327a9f9d28e359104cb8a04
Jonathan Espiard <jonathan.espiard@epita.fr>
2018-12-20 14:49:48 +0100

    [ADD] Can choose backers or not in the form

--- backend/src/form.js
+++ backend/src/form.js
@@ -1,9 +1,11 @@
 const request = require("request");
 const path = require('path')
 const arrays = require(path.join(__dirname,"../data_processing/files/arrays.js"));
-const uri = "https://ussouthcentral.services.azureml.net/workspaces/d404b57cee7b4af8a9be518cccea4e83/services/9e1fb031c33044c5b180377b1ab4ca9d/execute?api-version=2.0&details=true";
+const uriNoBackers = "https://ussouthcentral.services.azureml.net/workspaces/d404b57cee7b4af8a9be518cccea4e83/services/9e1fb031c33044c5b180377b1ab4ca9d/execute?api-version=2.0&details=true";
+const uriBackers = "https://ussouthcentral.services.azureml.net/workspaces/d404b57cee7b4af8a9be518cccea4e83/services/1832b8fc93f543bf8311e348bbce9c50/execute?api-version=2.0&details=true";
 const oldApikey ="ZGn4JRTBwiWNc8WENmQt7p1QK6zEiFP+c1XPjFrO/H3Ty1kn6n3VvY1LaOIUas0dnQsgRahFT7KgJkPpZPoXdw==";
-const apikey = "nGPIPahOtGew6WSzDeEcAaFTUZx4iDrn6VYjwr+837KEhX9UIW+PLO6g2uZgevvZQthIBnjNKftO3d2GRzH0nw==";
+const apikeyNoBackers = "nGPIPahOtGew6WSzDeEcAaFTUZx4iDrn6VYjwr+837KEhX9UIW+PLO6g2uZgevvZQthIBnjNKftO3d2GRzH0nw==";
+const apiKeyBackers = "moGO0IdruF4tkzkNjs2epw4Kt+6nQeIDCuE/sLFUFr1h9OeCGMycUyTGCkgi+y7GD0lUgbJIvvE6kjsU9ME8Kw==";
 
 module.exports = {
     post: post,
@@ -26,11 +28,38 @@ async function getCategory(req, res, next){
 function post(req,res,next){
     const category = req.body.category ? req.body.category : "";
     const state = "";
-    //const backers = req.body.backers ? req.body.backers : 0;
+    const backers = req.body.backers ? req.body.backers : 0;
     const country = req.body.country ? req.body.country : "";
     const days_before_deadline = req.body.days_before_deadline ? req.body.days_before_deadline : 0;
     const usd_goal_real = req.body.usd_goal_real ? req.body.usd_goal_real : 0;
-    let data = {
+    const uri = req.body.backers ? uriBackers : uriNoBackers;
+    const apikey = req.body.backers ? apiKeyBackers : apikeyNoBackers;
+
+    let data = req.body.backers ? {
+        "Inputs": {
+          "input1": {
+            "ColumnNames": [
+              "category",
+              "state",
+              "backers",
+              "country",
+              "usd_goal_real",
+              "days_before_deadline"
+            ],
+            "Values": [
+              [
+                category,
+                state,
+                parseInt(backers),
+                country,
+                parseInt(usd_goal_real),
+                parseInt(days_before_deadline)
+              ]
+            ]
+          }
+        },
+        "GlobalParameters": {}
+      } : {
         "Inputs": {
           "input1": {
             "ColumnNames": [
@@ -54,7 +83,7 @@ function post(req,res,next){
           }
         },
         "GlobalParameters": {}
-      }
+      };
 
     const options = {
         uri: uri,
@@ -72,8 +101,8 @@ function post(req,res,next){
             res.status(200).json({
                 "message":"request successful",
                 data: {
-                    accuracy: data.Values[0][6],
-                    value: data.Values[0][5]
+                    accuracy: req.body.backers ? data.Values[0][7] : data.Values[0][6],
+                    value:  req.body.backers ? data.Values[0][6] : data.Values[0][5],
                 }
             });
         } else {
